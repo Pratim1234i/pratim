26eff82792ebf1071c53d5cf3af692614541a23a
unknown <pratimchy48@gmail.com>
2019-03-23 20:36:58 -0800

    T

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,6 +83,7 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
+                <p>This</p>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
